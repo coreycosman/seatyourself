f13d261d13db3b06bf8f2bf5650fb73c869804cd
Michelle Cinello <m.cinello@gmail.com>
2017-10-23 16:34:02 -0400

    created user actions and new view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,22 @@
 class UsersController < ApplicationController
+
+  def create
+    @user = User.new(user_params)
+      if @user.save
+        redirect_to root_path
+      else
+        render :new
+      end
+  end
+
+  def new
+    @user = User.new
+  end
+
+private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :phone_number, :role, :password_digest)
+  end
+
 end
