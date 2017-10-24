a6062b09fd3241b408e1994a44324aa4007f7006
COREY COSMAN <cosman.corey@gmail.com>
2017-10-24 15:43:53 -0400

    the reservation form is functioning properly, is storing a restaurant_id and a user_id as a foreign key

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -6,10 +6,10 @@ class ReservationsController < ApplicationController
     @reservation.date = params[:reservation][:date]
     @reservation.time = params[:reservation][:time]
     @reservation.party_size = params[:reservation][:party_size]
-    # user_id = current_user.id
+
 
     @reservation.restaurant_id = @restaurant.id
-    @reservation.user_id = @user.id
+    @reservation.user_id = current_user.id
 
     if @reservation.save
       flash.notice = "Reservation has been successfully created."
