2479e9eff67b50d46415a18891c9d4cdb91544f3
COREY COSMAN <cosman.corey@gmail.com>
2017-10-24 10:17:51 -0400

    branch test

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,48 +1,50 @@
 class RestaurantsController < ApplicationController
+  class RestaurantsController < ApplicationController
 
-  def index
-    @restaurants = Restaurant.all
-  end
-
-  def new
-    @restaurant = Restaurant.new
-  end
-
-  def show
-    find_restaurant
-    @reservation = Reservation.new
-  end
-
-
-  def create
-    @restaurant = Restaurant.new(restaurant_params)
-    if @restaurant.save
-      redirect_to restaurant_path(@restaurant)
-    else
-      render :new
+    def index
+      @restaurants = Restaurant.all
     end
-  end
 
-  def edit
-    find_restaurant
-  end
-
-  def update
-    find_restaurant
-    if @restaurant.save(restaurant_params)
-      redirect_to restaurants_path
-    else
-      render :edit
+    def new
+      @restaurant = Restaurant.new
     end
+
+    def show
+      find_restaurant
+      @reservation = Reservation.find(params[:product_id])
+    end
+
+    def create
+      @restaurant = Restaurant.new(restaurant_params)
+      if @restaurant.save
+        redirect_to restaurant_path(@restaurant)
+      else
+        render :new
+      end
+    end
+
+    def edit
+      find_restaurant
+    end
+
+    def update
+      find_restaurant
+      if @restaurant.save(restaurant_params)
+        redirect_to restaurants_path
+      else
+        render :edit
+      end
+    end
+
+      private
+
+        def restaurant_params
+          params.require(:restaurant).permit(:name, :opening_time, :closing_time, :capacity, :user_id)
+        end
+
+        def find_restaurant
+          @restaurant = Restaurant.find(params[:id])
+        end
   end
 
-    private
-
-      def restaurant_params
-        params.require(:restaurant).permit(:name, :opening_time, :closing_time, :capacity, :user_id)
-      end
-
-      def find_restaurant
-        @restaurant = Restaurant.find(params[:id])
-      end
 end
