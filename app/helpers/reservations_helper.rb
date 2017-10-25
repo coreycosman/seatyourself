28d4b9d3104fc2b45b170d5b76fb0a312ae34d9a
COREY COSMAN <cosman.corey@gmail.com>
2017-10-24 22:34:22 -0400

    add the rest of the time options on the reservation form - this is done in the reservation_helper.rb file

--- app/helpers/reservations_helper.rb
+++ app/helpers/reservations_helper.rb
@@ -1,7 +1,19 @@
 module ReservationsHelper
 
   def reservation_times
-    {"1pm - 2pm" => 1}
+    {
+      "11am - 12pm" => 1,
+      "12pm - 1pm"  => 2,
+      "1pm - 2pm"   => 3,
+      "2pm - 3pm"   => 4,
+      "3pm - 4pm"   => 5,
+      "4pm - 5pm"   => 6,
+      "5pm - 6pm"   => 7,
+      "6pm - 7pm"   => 8,
+      "7pm - 8pm"   => 9,
+      "8pm - 9pm"   => 10,
+      "9pm - 10pm"  => 11,
+      "10pm - 11pm" => 12,
+    }
   end
-
 end
